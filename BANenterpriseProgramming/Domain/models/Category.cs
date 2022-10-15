96ae8889f16c6de03d47419975094b3af206aaee
Danilson Zammit <Danilson.Zammit.c10375@mcast.edu.mt>
2022-10-15 13:48:32 +0200

    First commit, created projects + starting adding models, no references yet

---
+++ BANenterpriseProgramming/Domain/models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ BANenterpriseProgramming/Domain/models/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain.models
+{
+    public class Item
+    {
+        // classes withing the domain models will be used
+        // to model/shape your database, a code first approach
+        [Key]
+        public int Id { get; set; }
+        public int Stock { get; set; }
+
+        [StringLength(100)]
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public double Price { get; set; }
+        
+        
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+
+        public virtual Category Category{get; set;}
+    }
+}
